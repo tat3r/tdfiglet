61e9847f1a64f1f37e1ed6463c72c45f9a22c0e2
tater <tater@trollforge.org>
2018-07-14 17:46:44 -0400

    added bsdl

--- tdfiglet.c
+++ tdfiglet.c
@@ -1,3 +1,17 @@
+/* Copyright (c) 2018 Trollforge. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Trollforge's name may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ */
+
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
